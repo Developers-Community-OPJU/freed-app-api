b4d1c988feab495f5eae153d986cd02ec9b11461
Roshan nahak <roshannahak1122@gmail.com>
2022-04-10 00:01:30 +0530

    variable naming correction

--- controllers/warden.dashboard.controller.js
+++ controllers/warden.dashboard.controller.js
@@ -4,11 +4,11 @@ module.exports = {
     GET_DASHBOARD_DATA : async (req, res) => {
         try{
             const totalStudents = await studentModel.Student.find({});
-            const totalStudentOfsHR1 = await studentModel.Student.find({residence: "HR1"});
-            const totalStudentOfsHR2 = await studentModel.Student.find({residence: "HR2"});
-            const totalStudentOfsHR3 = await studentModel.Student.find({residence: "HR3"});
+            const totalStudentsHR1 = await studentModel.Student.find({residence: "HR1"});
+            const totalStudentsHR2 = await studentModel.Student.find({residence: "HR2"});
+            const totalStudentsHR3 = await studentModel.Student.find({residence: "HR3"});
 
-            res.status(200).json({totalStudents : totalStudents.length, HR1 : totalStudentOfsHR1.length, HR2 : totalStudentOfsHR2.length, HR3 : totalStudentOfsHR3.length})
+            res.status(200).json({totalStudents : totalStudents.length, HR1 : totalStudentsHR1.length, HR2 : totalStudentsHR2.length, HR3 : totalStudentsHR3.length})
         }catch(error){
             console.error("error no 896" + error);
             res.send(error);
