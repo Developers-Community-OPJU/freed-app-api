7122a9feb566269fbf1e78418f53a2c432509fac
Roshan nahak <roshannahak1122@gmail.com>
2022-04-09 23:31:31 +0530

    dashboard api

--- controllers/warden.dashboard.controller.js
+++ controllers/warden.dashboard.controller.js
@@ -3,10 +3,14 @@ const studentModel = require("../models/StudentModel");
 module.exports = {
     GET_DASHBOARD_DATA : async (req, res) => {
         try{
-            studentModel.Student.find().then(result => 
-                {res.status(200).json({totalStudents : result.length})})
+            const totalStudents = await studentModel.Student.find({});
+            const totalStudentOfsHR1 = await studentModel.Student.find({residence: "HR1"});
+            const totalStudentOfsHR2 = await studentModel.Student.find({residence: "HR2"});
+            const totalStudentOfsHR3 = await studentModel.Student.find({residence: "HR3"});
+
+            res.status(200).json({totalStudents : totalStudents.length, HR1 : totalStudentOfsHR1.length, HR2 : totalStudentOfsHR2.length, HR3 : totalStudentOfsHR3.length})
         }catch(error){
-            console.error(error);
+            console.error("error no 896" + error);
             res.send(error);
         }
     }
