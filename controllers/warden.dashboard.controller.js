60caf122f4b2f7878da52a8237408bd3233a1432
Developer's Community @OPJU <dco.opju@gmail.com>
2022-04-15 23:43:09 +0530

    FDesk dashboard api update - checlist data

--- controllers/warden.dashboard.controller.js
+++ controllers/warden.dashboard.controller.js
@@ -1,17 +1,38 @@
 const studentModel = require("../models/StudentModel");
+const { Checklist } = require("../models/Checklist");
+const { Checkout } = require("../models/Checkout");
+const { Checkin } = require("../models/Checkin");
 
 module.exports = {
-    GET_DASHBOARD_DATA : async (req, res) => {
-        try{
-            const totalStudents = await studentModel.Student.find({});
-            const totalStudentsHR1 = await studentModel.Student.find({residence: "HR1"});
-            const totalStudentsHR2 = await studentModel.Student.find({residence: "HR2"});
-            const totalStudentsHR3 = await studentModel.Student.find({residence: "HR3"});
+  GET_DASHBOARD_DATA: async (req, res) => {
+    try {
+      const totalStudents = await studentModel.Student.find({});
+      const totalStudentsHR1 = await studentModel.Student.find({
+        residence: "HR1",
+      });
+      const totalStudentsHR2 = await studentModel.Student.find({
+        residence: "HR2",
+      });
+      const totalStudentsHR3 = await studentModel.Student.find({
+        residence: "HR3",
+      });
+      //   gateway
+      const checklist = await Checklist.find({});
+      const checkouts = await Checkout.find({});
+      const checkins = await Checkin.find({});
 
-            res.status(200).json({totalStudents : totalStudents.length, HR1 : totalStudentsHR1.length, HR2 : totalStudentsHR2.length, HR3 : totalStudentsHR3.length})
-        }catch(error){
-            console.error("error no 896" + error);
-            res.send(error);
-        }
+      res.status(200).json({
+        totalStudents: totalStudents.length,
+        HR1: totalStudentsHR1.length,
+        HR2: totalStudentsHR2.length,
+        HR3: totalStudentsHR3.length,
+        checklist: checklist.length,
+        checkouts: checkouts.length,
+        checkins: checkins.length,
+      });
+    } catch (error) {
+      console.error("error no 896" + error);
+      res.send(error);
     }
-}
+  },
+};
