7e90f182434a5b3cd85e4d11165f473591720185
amankumar430206 <amankumar430206@gmail.com>
2022-05-06 13:01:50 +0530

    Revert "added student name property and profile img when student checkout"

--- controllers/gate.controller.js
+++ controllers/gate.controller.js
@@ -29,7 +29,7 @@ module.exports = {
           record: inChecklist.record,
         });
 
-        //find student profile picture flom checkout list
+        //find student profile picture
         let studentProfile = await Student.findOne({ _id: inCheckout.student });
 
         let removed = await Checkout.findOneAndRemove({
@@ -46,7 +46,6 @@ module.exports = {
           await checkin.save();
           return res.json({
             checkedin: true,
-            studentName: studentProfile.firstName+" "+studentProfile.lastName,
             profile: studentProfile.profile,
             msg: "Welcome, Great to have you back!",
             success: true,
@@ -63,14 +62,9 @@ module.exports = {
           record: inChecklist.record,
         });
 
-        //find student profile picture from checklist
-        let studentProfile = await Student.findOne({ _id: inChecklist.student });
-
         await checkout.save();
         return res.status(200).json({
           checkedout: true,
-          studentName: studentProfile.firstName+" "+studentProfile.lastName,
-          profile: studentProfile.profile,
           msg: "Hurray! Have a Great Journey :)",
           success: true,
           data: {
